c4144a3d43c45ff9b8ff9f08ac65502afd522187
Mochammad Alfin Faturochman <82384511+alfinfaturochmanm@users.noreply.github.com>
2022-11-29 22:58:41 +0700

    Service Providers

---
+++ src/app/providers/InventoryPackageServiceProviders.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AlfinFaturochmanm\InventoryPackage\App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class InventoryPackageServiceProviders extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
